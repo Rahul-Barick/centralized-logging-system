5da3eda35a01171e046cfd58fe8e44d7f4550668
Rahul-Barick <libra2rahul@gmail.com>
2021-02-09 03:14:44 +0530

    updated index file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-var cluster = require("cluster");
-var numCPUs = require("os").cpus().length;
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
 
 if (cluster.isMaster) {
   // Fork workers.
-  for (var i = 0; i < numCPUs; i++) {
+  for (let i = 0; i < numCPUs; i+=1) {
     cluster.fork();
   }
   cluster.on("exit", function (worker, code, signal) {
